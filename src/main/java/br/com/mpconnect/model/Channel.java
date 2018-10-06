8809c7610351a38740e6d180b730c6efeb2c0b45
Unknown <andremo2017@gmail.com>
2018-10-06 19:47:43 -0300

    settando o canal da venda da b2w

--- src/main/java/br/com/mpconnect/model/Channel.java
+++ src/main/java/br/com/mpconnect/model/Channel.java
@@ -3,12 +3,32 @@ package br.com.mpconnect.model;
 public enum Channel 
 {
 	ML{
-		public Origem getOrigem(){return new Origem(1L);};
+		public Origem getOrigem(){return new Origem(1L,"ML");};
 	},
 	B2W
 	{
-		public Origem getOrigem(){return new Origem(3L);};
+		public Origem getOrigem(){return new Origem(3L,"B2W");};
+	},
+	AMERICANAS
+	{
+		public Origem getOrigem(){return new Origem(4L,"Lojas Americanas");};
+	},
+	SUBMARINO
+	{
+		public Origem getOrigem(){return new Origem(5L,"Submarino");};
+	},
+	SHOPTIME
+	{
+		public Origem getOrigem(){return new Origem(6L,"Shoptime");};
 	};
 
 	public abstract Origem getOrigem();
+		
+	public static Channel lookup(String channelName)
+	{
+		for(Channel channel : Channel.values())
+			if(channel.getOrigem().getNome().equals(channelName))
+				return channel;
+		return Channel.ML;
+	}
 }
